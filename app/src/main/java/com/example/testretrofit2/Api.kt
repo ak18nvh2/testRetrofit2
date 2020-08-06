674dedd916c0c22520c3188a888cf10aa258f3b5
ak18nvh2 <50037709+ak18nvh2@users.noreply.github.com>
2020-08-06 14:51:24 +0700

    add headers to api

--- app/src/main/java/com/example/testretrofit2/Api.kt
+++ app/src/main/java/com/example/testretrofit2/Api.kt
@@ -2,8 +2,12 @@ package com.example.testretrofit2
 
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Header
+import retrofit2.http.Headers
 
 interface Api {
+    @Headers(value = ["Accept: application/json",
+        "Content-type:application/json"])
     @GET("employees/")
     fun getEmployees(): Call<FileJson>
 
